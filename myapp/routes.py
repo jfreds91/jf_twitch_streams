2bb27d5b2249266abe143bea28735618840ffb37
jesse fredrickson <jfreds@bu.edu>
2019-05-14 23:50:17 +0000

    first commit

---
+++ myapp/routes.py
@@ -0,0 +1,25 @@
+from myapp import app
+import json, plotly
+from flask import render_template
+from wrangling_scripts.wrangle_data import return_figures, return_data
+
+@app.route('/')
+@app.route('/index')
+def index():
+
+    data = return_data()
+    testdata = str(data)
+    
+    figures = return_figures()
+
+    # plot ids for the html id tag
+    ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
+
+    # Convert the plotly figures to JSON for javascript in html template
+    figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('index.html',
+                           data = data,
+                           testdata = testdata,
+                           ids=ids,
+                           figuresJSON=figuresJSON)
