7833df3cd2e7fc48e8417edc23810c656351fd3f
jesse <jfreds@bu.edu>
2019-05-25 19:31:08 -0400

    fix: trying different redis port

--- worker.py
+++ worker.py
@@ -6,7 +6,8 @@ import os
 listen = ['high', 'default', 'low']
 
 # unsure what port to be using. Might actually be 29909
-redis_url = os.getenv('REDIST_URL', 'redis://localhost:6379')
+# added /0 per stack overflow?
+redis_url = os.getenv('REDIST_URL', 'redis://localhost:6379/0')
 
 rq_conn = redis.from_url(redis_url)
 
