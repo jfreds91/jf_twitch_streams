d02726f4e7b45a3e0cd35c1dba74f727ca7c3345
jesse <jfreds@bu.edu>
2019-05-25 20:57:47 -0400

    fix: added type conversion and error printouts to sql_update.py

--- wrangling_scripts/sql_update.py
+++ wrangling_scripts/sql_update.py
@@ -52,71 +52,75 @@ def kickoff_sql_thread(max_size = 9000, sleep_time = 900):
         sleep_time (int): number of seconds between twitch queries
     RETURNS: none
     '''
-    
-    def check_table_size(engine, max_size):
-    # this function checks the number of rows in the table and deletes the oldest entires
-    # if over max_size threshold
-        c = engine.execute('SELECT COUNT(*) FROM "Games";')
-        num_rows = c.fetchall()[0][0]
-        c.close()
-        #print('table contains {} rows'.format(num_rows))
-
-        while num_rows >= max_size:
-            #print('\tmax table size reached, deleting oldest entries')
-            df = pd.read_sql_table('Games', con = engine)
-            df['time'] = pd.to_datetime(df['time'])
-            mintime = df['time'].min().strftime('%Y-%m-%d %H:%M:%S.%f')
-
-            del_statement = games.delete().where(games.c.time == mintime)
-            connection = engine.connect()
-            connection.execute(del_statement)
-            connection.close()
-
-            c = engine.execute('SELECT COUNT(*) FROM "Games"')
-            num_rows = c.fetchall()[0][0]
+    try:
+        def check_table_size(engine, max_size):
+        # this function checks the number of rows in the table and deletes the oldest entires
+        # if over max_size threshold
+            c = engine.execute('SELECT COUNT(*) FROM "Games";')
+            num_rows = int(c.fetchall()[0][0])
             c.close()
-        
-    
-#     def constant_query(engine, max_size, sleep_time):
-#         '''
-#         function which continuously adds to the Games table using the given SQL engine
+            #print('table contains {} rows'.format(num_rows))
 
-#         INPUTS:
-#             v5_client: the twitch v5 client
-#             engine: sql alchemy database engine
-#             max_size (int): the size at which the function begins deleting rows. Note that the actual
-#                             size of the database will be max_size + query num (10)
-#             sleep_time (int): the API query interval in seconds
-#         '''
+            while num_rows >= max_size:
+                #print('\tmax table size reached, deleting oldest entries')
+                df = pd.read_sql_table('Games', con = engine)
+                df['time'] = pd.to_datetime(df['time'])
+                mintime = df['time'].min().strftime('%Y-%m-%d %H:%M:%S.%f')
 
-#         while True:
-#             if stop_threads:
-#                 break
+                del_statement = games.delete().where(games.c.time == mintime)
+                connection = engine.connect()
+                connection.execute(del_statement)
+                connection.close()
 
-#             check_table_size(engine, max_size)
-#             df = get_top_games() # this function is from twitch_api_funcs, imported
-#             df.to_sql('Games', engine, index=False, if_exists = 'append')
+                c = engine.execute('SELECT COUNT(*) FROM "Games"')
+                num_rows = c.fetchall()[0][0]
+                c.close()
 
-#             print('timestamp: {}, length: {}'.format(datetime.datetime.now(), len(df)))
-#             time.sleep(sleep_time)
 
-    # prepare to start thread
-    engine = get_sql_engine()
-#     global stop_threads
-#     stop_threads = False
+    #     def constant_query(engine, max_size, sleep_time):
+    #         '''
+    #         function which continuously adds to the Games table using the given SQL engine
 
-    # create and start thread
-#     t1 = threading.Thread(target=constant_query, args = (engine, max_size, sleep_time))
-#     t1.start()
+    #         INPUTS:
+    #             v5_client: the twitch v5 client
+    #             engine: sql alchemy database engine
+    #             max_size (int): the size at which the function begins deleting rows. Note that the actual
+    #                             size of the database will be max_size + query num (10)
+    #             sleep_time (int): the API query interval in seconds
+    #         '''
 
-    while True:
-        
-        check_table_size(engine, max_size)
-        
-        print('############################## Querying API #################################')
-        df = get_top_games()
-        df.to_sql('Games', engine, index=False, if_exists = 'append')
-        time.sleep(sleep_time)
+    #         while True:
+    #             if stop_threads:
+    #                 break
+
+    #             check_table_size(engine, max_size)
+    #             df = get_top_games() # this function is from twitch_api_funcs, imported
+    #             df.to_sql('Games', engine, index=False, if_exists = 'append')
+
+    #             print('timestamp: {}, length: {}'.format(datetime.datetime.now(), len(df)))
+    #             time.sleep(sleep_time)
+
+        # prepare to start thread
+        engine = get_sql_engine()
+    #     global stop_threads
+    #     stop_threads = False
+
+        # create and start thread
+    #     t1 = threading.Thread(target=constant_query, args = (engine, max_size, sleep_time))
+    #     t1.start()
+
+        while True:
+
+            check_table_size(engine, max_size)
+
+            print('############################## Querying API #################################')
+            df = get_top_games()
+            df.to_sql('Games', engine, index=False, if_exists = 'append')
+            time.sleep(sleep_time)
+    except Exception as e:
+        print(e)
+        print('num_rows = {}, type = {}'.format(num_rows, type(num_rows)))
+        print('max_size = {}, type = {}'.format(max_size, type(max_size)))
 
     
 def get_sql_size():
